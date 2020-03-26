f281ce31e26b11fd992b9c5833424e80552de2a2
Evandy <andi_evandy@yahoo.co.id>
2020-03-26 18:37:11 +0700

    hashpassword kurang parameter

--- helpers/hashPassword.js
+++ helpers/hashPassword.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcryptjs');
 
 function hashPassword(password) {
-    return bcrypt.hash(password);
+    return bcrypt.hash(password, 10);
 }
 
 function hashPasswordSync(password) {
